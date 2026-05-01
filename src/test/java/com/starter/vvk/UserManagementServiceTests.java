2311069ef72e30f6d66e704b834ac89fd1142137
loofy <loofidmonkey@gmail.com>
2026-05-01 22:04:21 +0530

    small change in file name

--- src/test/java/com/starter/vvk/UserManagementServiceTests.java
+++ src/test/java/com/starter/vvk/UserManagementServiceTests.java
@@ -1,10 +1,10 @@
-package com.starter.vvk;
+package com.starter.usermanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VvkApplicationTests {
+class UserManagementServiceTests {
 
 	@Test
 	void contextLoads() {
